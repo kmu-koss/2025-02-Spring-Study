7018e54575989261d978fcf1ca9f0203bec81b7b
hanyj0317 <hyj0931@naver.com>
2025-11-12 12:49:48 +0900

    feat: 두번째 과제 Spring Boot 프로젝트 설정

---
+++ HanYeojin/Assignment2/yeojin/cafe-server/src/main/java/com/yeojin/cafe/CafeServerApplication.java
@@ -0,0 +1,13 @@
+package com.yeojin.cafe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CafeServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CafeServerApplication.class, args);
+	}
+
+}
